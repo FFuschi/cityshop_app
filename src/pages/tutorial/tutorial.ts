9a7df00ac38c1bdb7912beb59cf28216184ba2aa
pio-9 <pio-9@DESKTOP-DO1VFR1>
2017-09-16 11:06:58 +0200

    Update

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../home/home';
 //Providers
 import { AccesspersistanceProvider } from '../../providers/accesspersistance/accesspersistance';
 import { AccountProvider } from '../../providers/account/account';
+import { DictionaryServiceProvider } from '../../providers/dictionary-service/dictionary-service';
 
 @Component({
     selector: 'page-tutorial',
@@ -20,17 +21,17 @@ export class TutorialPage {
         {
             title: "Markers",
             description: "Tappando in uno dei marker intorno a te potrai visualizzare le informazioni del negozio che vende prodotti di tuo interesse.",
-            image: "assets/images/tutorial1.png",
+            image: "assets/images/tutorial1.gif",
         },
         {
             title: "Profilo",
             description: "Per accedere al tuo profilo tappa sull'immagine del tuo profilo!",
-            image: "assets/images/tutorial2.png",
+            image: "assets/images/tutorial2.gif",
         },
         {
             title: "Modifica le tue preferenze",
             description: "Puoi modificare le tue preferenze di brands e categorie all'interno del tuo profilo tappando nelle zone sopra descritte",
-            image: "assets/images/tutorial3.png",
+            image: "assets/images/tutorial3.gif",
         }
     ];
 
@@ -38,9 +39,31 @@ export class TutorialPage {
                 public plt: Platform,
                 private statusBar: StatusBar,
                 public sAccount: AccountProvider,
-                public aPersistance: AccesspersistanceProvider
+                public aPersistance: AccesspersistanceProvider,
+                public sTranslate: DictionaryServiceProvider
                 ) {
 
+    }
+    
+    ngOnInit(){
+        
+        this.slides = [
+            {
+                title: this.sTranslate.get("MARKERS"),
+                description: this.sTranslate.get("TUT1"),
+                image: "assets/images/tutorial1.gif",
+            },
+            {
+                title: this.sTranslate.get("PROFILE"),
+                description: this.sTranslate.get("TUT2"),
+                image: "assets/images/tutorial2.gif",
+            },
+            {
+                title: this.sTranslate.get("MODPREF"),
+                description: this.sTranslate.get("TUT3"),
+                image: "assets/images/tutorial3.gif",
+            }
+        ];
     }
     
     ionViewDidLoad() {
